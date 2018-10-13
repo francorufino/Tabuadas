fbdda00d1fdbf05d671a5b635f14bdce200149ae
francorufino <campbell.andreza@gmail.com>
2018-10-12 22:39:34 -0300

    uploading tabuadas - work in progress

---
+++ Tabuadas/app/src/main/java/com/example/andreza/tabuadas/TabuadasActivity.java
@@ -0,0 +1,70 @@
+package com.example.andreza.tabuadas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TabuadasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabuadas);
+    }
+    public void btn0Escolhido(View view) {
+    }
+
+    public void btn1Escolhido(View view) {
+    }
+
+    public void btn2Escolhido(View view) {
+    }
+
+    public void btn3Escolhido(View view) {
+    }
+
+    public void btn4Escolhido(View view) {
+    }
+
+    public void btn5Escolhido(View view) {
+    }
+
+    public void btn6Escolhido(View view) {
+    }
+
+    public void btn7Escolhido(View view) {
+    }
+
+    public void btn8Escolhido(View view) {
+    }
+
+    public void btn9Escolhido(View view) {
+    }
+
+    public void btn10Escolhido(View view) {
+    }
+
+    public void btn11Escolhido(View view) {
+    }
+
+    public void btn12Escolhido(View view) {
+    }
+
+    public void btn13Escolhido(View view) {
+    }
+
+    public void btn14Escolhido(View view) {
+    }
+
+    public void btn15Escolhido(View view) {
+    }
+
+    public void btnIniciarEscolhido(View view) {
+    }
+
+    public void btnVoltarEscolhido(View view) {
+    }
+
+
+}
+
